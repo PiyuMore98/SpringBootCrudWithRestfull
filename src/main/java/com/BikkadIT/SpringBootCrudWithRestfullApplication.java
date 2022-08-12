70aae13a943aa57945b760c7d445be0c91f98bf2
PiyuMore98 <poojamore25041998@gmail.com>
2022-08-12 11:59:06 +0530

    task added to central repository

---
+++ src/main/java/com/BikkadIT/SpringBootCrudWithRestfullApplication.java
@@ -0,0 +1,13 @@
+package com.BikkadIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudWithRestfullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudWithRestfullApplication.class, args);
+	}
+
+}
